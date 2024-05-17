cb7012d08481d16f173929f8a87d416c5e2d5268
Milox21 <milox212@gmail.com>
2024-05-17 22:29:50 +0200

    Bugfix, fixed bug with json extraction

--- CatFactWritter/CatFactModel.cs
+++ CatFactWritter/CatFactModel.cs
@@ -15,11 +15,13 @@ namespace CatFactWritter
         /// <summary>
         /// Gets or sets the cat fact.
         /// </summary>
+        [JsonPropertyName("fact")]
         public string Fact { get; set; }
 
         /// <summary>
         /// Gets or sets the length of the cat fact.
         /// </summary>
+        [JsonPropertyName("length")]
         public int Length { get; set; }
     }
 }
